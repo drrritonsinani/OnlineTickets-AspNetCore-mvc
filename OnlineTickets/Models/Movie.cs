d6a34e08f52329c92063c667bf2099cbd952106e
sinan <sinan@DESKTOP-L1VO641>
2022-04-19 01:28:01 +0200

    Models; first Migration; DbInitializer

---
+++ OnlineTickets/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using OnlineTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+        public string MovieUrl { get; set; }
+        public MovieCategory Category { get; set; }
+
+
+        //Relationships
+        public List<ActorMovie> ActorsMovies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+    }
+}
